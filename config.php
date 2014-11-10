984eab057758a5bceb13afeb8090fb3151fefbae
Ernst-Jan van Woerden <hello@seriousness.nl>
2014-11-10 22:30:58 +0800

    updated config

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+date_default_timezone_set('America/New_York');
+
+$twitter = array(
+    'consumer_key' =>           'MMHbSkI0dcEan9bGVFm4fNL2F',
+    'consumer_secret' =>        'E1JSMf9yqU61wDwithFv8VYz2fmpfLq0zOHXlyRvTxyewm6H2y',
+    'access_token' =>           '48034400-rze3cdjbZVAtQEkORK41GwSspPhs2cIKwBuwbxG4N',
+    'access_token_secret' =>    'ydbfngRLz9PpXy3zQc1V6wSa6fZ72SoN8nXhM5bcYjmvd'
+);
+
+$instagram = array(
+    'client_id' =>       'c6cfbbcd9a494e17a177d635b55fb88f',
+    'client_secret' =>  '1da33961481b4d4b8e47510ef0889014',
+    'website_url' =>    'http://seriousness.nl/gifashit/test',
+    'redirect_uri' =>   'http://seriousness.nl/gifashit/test',
+    'access_token' =>   'YOUR_ACCESS_TOKEN'
+);
+
+$db = array(
+    'host' =>       'server101.hosting2go.nl',
+    'user' =>       'ernst',
+    'password' =>   '2Fserious',
+    'name' =>       'gifashit'
+);
+
+$hashtag = 'winning';
+
+?>
